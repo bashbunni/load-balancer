4be628c25e02db887c53d2328b33d17ebb28bbf9
bashbunni <bunni@bashbunni.dev>
2026-05-05 11:36:50 -0700

    feat: add health check

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,53 @@ use serde::Deserialize;
 use std::error;
 use std::fmt;
 use std::fs::File;
+use std::io;
 use std::io::{Read, Write};
-use std::net::TcpStream;
-// use std::net::{TcpListener, TcpStream};
+use std::net::SocketAddr;
+use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+
+fn main() {
+    // start
+    // TODO handle errors
+    // TODO make async
+    // TODO connection check for port, not just ping server ip
+    let server: Vec<Server> = get_servers_from_yaml().unwrap();
+    let servers: Vec<Server> = server
+        .into_iter()
+        .map(|mut server| {
+            let socket_addr: SocketAddr = server.name.parse().expect("invalid address");
+            match TcpStream::connect_timeout(&socket_addr, Duration::from_millis(20000)) {
+                Ok(_) => server.can_connect = true,
+                Err(e) if e.kind() == std::io::ErrorKind::ConnectionRefused => {
+                    server.can_connect = false
+                }
+                Err(_) => server.can_connect = false,
+            }
+            server
+        })
+        .collect();
+    println!("{servers:?}");
+}
+
+// TODO use this once we're ready to start receiving connections.
+fn start_server() -> Result<(), anyhow::Error> {
+    let listener = TcpListener::bind("127.0.0.1:43000")?;
+    println!("Server listening on 127.0.0.1:43000");
+
+    // Accept connections and handle them
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr()?);
+                handle_client(stream)?;
+            }
+            Err(e) => eprintln!("Error accepting connection: {}", e),
+        }
+    }
+
+    Ok(())
+}
 
 fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
     let mut buffer = [0; 512];
@@ -27,31 +71,8 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
     Ok(())
 }
 
-fn main() {
-    /*
-        // Bind to localhost:8080
-        let listener = TcpListener::bind("127.0.0.1:43000")?;
-        println!("Server listening on 127.0.0.1:43000");
-
-        // Accept connections and handle them
-        for stream in listener.incoming() {
-            match stream {
-                Ok(stream) => {
-                    println!("New connection: {}", stream.peer_addr()?);
-                    handle_client(stream)?;
-                }
-                Err(e) => eprintln!("Error accepting connection: {}", e),
-            }
-        }
-
-        Ok(())
-    */
-    get_servers().unwrap();
-}
-
-// get list of available servers
-// via yaml file
 // server state tracking: ip:port, up? can we connect to it?, number of connections
+// startup behaviour: read yaml, check server is up (ping), start routing traffic
 
 #[derive(Debug, Deserialize, Default)]
 struct Server {
@@ -60,7 +81,7 @@ struct Server {
     can_connect: bool,
 }
 
-fn get_servers() -> Result<Vec<Server>, anyhow::Error> {
+fn get_servers_from_yaml() -> Result<Vec<Server>, anyhow::Error> {
     // read yaml
     let file: File = File::open("./src/default.yml")?;
 
@@ -70,7 +91,7 @@ fn get_servers() -> Result<Vec<Server>, anyhow::Error> {
     let servers: Vec<Server> = server_names
         .into_iter()
         .map(|name| Server {
-            name: name.to_string(),
+            name: name,
             ..Default::default()
         })
         .collect();
